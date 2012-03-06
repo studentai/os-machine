4cec5a3947fabd25f1db082dd0e67c65517c2b05
Eligijus <estarinskas@gmail.com>
2012-03-06 22:24:29 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("Hello World elliga response");
+		System.out.println("Hello World elliga response kill all human");
 	}
 }
