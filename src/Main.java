566a62e153d9cb6142658357bea6e1e4a646805f
studentai <estarinskas@gmail.com>
2012-03-07 18:08:19 +0200

    eleventh commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("Hello World wth motherhooker FAGS ;D ?");
+		System.out.println("Hello World wth motherhooker FAGS douchebags dicks asses ?");
 	}
 }
