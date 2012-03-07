e589818db590237f7a5df7c2774c9020cd9cf0c1
studentai <estarinskas@gmail.com>
2012-03-07 18:30:47 +0200

    susitvarkiau

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("Hello World susitvarkiau");
+		System.out.println("Hello World susitvarkiau ble su tais proxiais");
 	}
 }
