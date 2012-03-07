a71fed61c661823a9c238b9940c5b6e4ee36cc27
studentai <estarinskas@gmail.com>
2012-03-07 23:32:54 +0200

    labadiena

--- src/VirtualMachine.java
+++ src/VirtualMachine.java
@@ -4,11 +4,37 @@ public class VirtualMachine {
     private byte[] SF; 												// status flag (!3 baitas Loginis C registras!)
     private byte[] P;  												// bendro naudojimo registras
     private byte[] R;												// bendro naudojimo registras
+    
 	public byte[] getIC() {
 		return IC;
 	}
-	public void setIC(byte[] iC) {
-		IC = iC;
+	
+	public void setIC(byte[] IC) {
+		this.IC = IC;
+	}
+	
+	public byte[] getSF() {
+		return SF;
+	}
+	
+	public void setSF(byte[] SF) {
+		this.SF = SF;
+	}
+	
+	public byte[] getP() {
+		return P;
+	}
+	
+	public void setP(byte[] p) {
+		P = p;
+	}
+	
+	public byte[] getR() {
+		return R;
+	}
+	
+	public void setR(byte[] r) {
+		R = r;
 	}
     
     
