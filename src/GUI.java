4a11dbc0a19443bfaea77012b4cd93a55fd00fc5
studentai <estarinskas@gmail.com>
2012-03-12 17:46:16 +0200

    uzpiso

--- src/GUI.java
+++ src/GUI.java
@@ -19,7 +19,7 @@ import org.dyno.visual.swing.layouts.Constraints;
 import org.dyno.visual.swing.layouts.GroupLayout;
 import org.dyno.visual.swing.layouts.Leading;
 
-
+//bbzn
 //VS4E -- DO NOT REMOVE THIS LINE!
 public class GUI extends JFrame {
 
@@ -29,37 +29,101 @@ public class GUI extends JFrame {
 	private JButton stepButton;
 	private JButton changeRegistersButton;
 	private JButton changeMemoryButton;
-	
+
 	private JTable realMemoryTable;
 	private JTable registersTable;
 	private JTable flagsTable;
 	private JTable commandsTable;
-	
+
 	private JScrollPane jScrollPane0;
 	private JScrollPane jScrollPane2;
 	private JScrollPane jScrollPane3;
 	private JScrollPane jScrollPane4;
 	private JScrollPane jScrollPane5;
 	private JLabel jLabel0;
-	
+
 	public static String[][] values;
 	public static Integer[][] values2;
-	
+
 	private JTextArea jTextArea0;
-	private JTextField jTextField0;
-	private JLabel jLabel1;
-	private JLabel jLabel2;
-	private JTextField jTextField1;
-	private JPanel jPanel0;
+	private JPanel jPanel1;
 	private JFrame framek;
 	private JFrame frameks;
 	private JButton okButton;
 	private JScrollPane jScrollPane1;
 	private JButton jButton5;
-	private JPanel jPanel1;
-	
+
+	private RealMachine realMachine;
+	private JTextField jTextField6;
+	private JLabel jLabel3;
+	private JTextField jTextField4;
+	private JTextField jTextField3;
+	private JTextField jTextField2;
+	private JTextField jTextField5;
+	private JLabel jLabel4;
+	private JPanel jPanel2;
+
 	public GUI() {
 		initComponents();
+	}
+
+	public void setRealMachine(RealMachine realMachine){
+		this.realMachine = realMachine;
+	}
+
+	public RealMachine getRealMachine(){
+		return realMachine;
+	}
+
+	public void updateRegistersValues(){
+
+		for(int i = 0; i<4; i++){
+			registersTable.setValueAt(realMachine.getPTR()[i], 0, i+1);
+			registersTable.setValueAt(realMachine.getR()[i], 1, i+1);
+			registersTable.setValueAt(realMachine.getR()[i], 2, i+1);
+		}
+		for(int i = 0; i < 2; i++){
+			registersTable.setValueAt(realMachine.getIC()[i], 3, i+1);
+			registersTable.setValueAt(realMachine.getSF()[i], 4, i+1);
+			registersTable.setValueAt(realMachine.getSM()[i], 5, i+1);
+		}
+
+		registersTable.setValueAt(realMachine.getPI(), 6, 1);
+		registersTable.setValueAt(realMachine.getSI(), 7, 1);
+		registersTable.setValueAt(realMachine.getChnl(), 8, 1);
+
+	}
+
+	public void updateRealMemory(){
+		byte[][] memory = realMachine.getRealMemory().getMemory();
+		for(int i = 0; i<memory.length; i++){
+			realMemoryTable.setValueAt(Converter.ByteToChar(memory[i][0]), i, 1);
+			realMemoryTable.setValueAt(Converter.ByteToChar(memory[i][1]), i, 2);
+			realMemoryTable.setValueAt(Converter.ByteToChar(memory[i][2]), i, 3);
+			realMemoryTable.setValueAt(Converter.ByteToChar(memory[i][3]), i, 4);
+		}
+	}
+
+	public void updateFlags(){
+		byte[] flag = realMachine.getSF();
+		String firstByte = Integer.toBinaryString(flag[0]);
+		String secondByte = Integer.toBinaryString(flag[1]);
+
+		if(firstByte.length() == 3) firstByte = "0"+firstByte;
+		if(firstByte.length() == 2) firstByte = "00"+firstByte;
+		if(firstByte.length() == 1) firstByte = "000"+firstByte;
+		if(secondByte.length() == 3) secondByte = "0"+secondByte;
+		if(secondByte.length() == 2) secondByte = "00"+secondByte;
+		if(secondByte.length() == 1) secondByte = "000"+secondByte;
+
+		char[] charsArray1 = firstByte.toCharArray();
+		char[] charsArray2 = secondByte.toCharArray();
+
+		for(int i = 0; i<firstByte.length(); i++){
+			flagsTable.setValueAt(charsArray1[i], i, 1);
+			flagsTable.setValueAt(charsArray2[i], i+4, 1);
+		}
+
 	}
 
 	private void initComponents() {
@@ -78,6 +142,107 @@ public class GUI extends JFrame {
 		setSize(775, 513);
 	}
 
+	private JPanel getJPanel2() {
+		if (jPanel2 == null) {
+			jPanel2 = new JPanel();
+			jPanel2.setLayout(new GroupLayout());
+			jPanel2.add(getJLabel3(), new Constraints(new Leading(10, 10, 10), new Leading(17, 12, 12)));
+			jPanel2.add(getJTextField4(), new Constraints(new Leading(160, 24, 12, 12), new Leading(47, 12, 12)));
+			jPanel2.add(getJTextField3(), new Constraints(new Leading(130, 24, 12, 12), new Leading(47, 12, 12)));
+			jPanel2.add(getJTextField2(), new Constraints(new Leading(100, 24, 12, 12), new Leading(47, 12, 12)));
+			jPanel2.add(getJTextField5(), new Constraints(new Leading(70, 24, 12, 12), new Leading(47, 12, 12)));
+			jPanel2.add(getJLabel4(), new Constraints(new Leading(10, 12, 12), new Leading(49, 12, 12)));
+			jPanel2.add(getOkButton(), new Constraints(new Leading(63, 10, 10), new Leading(85, 12, 12)));
+			jPanel2.add(getJTextField6(), new Constraints(new Leading(70, 39, 10, 10), new Leading(17, 12, 12)));
+		}
+		return jPanel2;
+	}
+
+	private JLabel getJLabel4() {
+		if (jLabel4 == null) {
+			jLabel4 = new JLabel();
+			jLabel4.setText("Reik?m?");
+		}
+		return jLabel4;
+	}
+
+	private JTextField getJTextField5() {
+		if (jTextField5 == null) {
+			jTextField5 = new JTextField();
+			jTextField5.setText("");
+		}
+		return jTextField5;
+	}
+
+	private JTextField getJTextField2() {
+		if (jTextField2 == null) {
+			jTextField2 = new JTextField();
+			jTextField2.setText("");
+		}
+		return jTextField2;
+	}
+
+	private JTextField getJTextField3() {
+		if (jTextField3 == null) {
+			jTextField3 = new JTextField();
+			jTextField3.setText("");
+		}
+		return jTextField3;
+	}
+
+	private JTextField getJTextField4() {
+		if (jTextField4 == null) {
+			jTextField4 = new JTextField();
+			jTextField4.setText("");
+		}
+		return jTextField4;
+	}
+
+	private JLabel getJLabel3() {
+		if (jLabel3 == null) {
+			jLabel3 = new JLabel();
+			jLabel3.setText("Adresas");
+		}
+		return jLabel3;
+	}
+
+	private JTextField getJTextField6() {
+		if (jTextField6 == null) {
+			jTextField6 = new JTextField();
+			jTextField6.setDocument(new JTextFieldLimit(4));
+			jTextField6.setText("");
+		}
+		return jTextField6;
+	}
+
+	private JButton getJButton5() {
+		if (jButton5 == null) {
+			jButton5 = new JButton();
+			jButton5.addActionListener(new ActionListener() {
+
+				public void actionPerformed(ActionEvent event) {
+					ok2ButtonActionActionPerformed(event);
+				}
+			});
+			jButton5.setText("Gerai");
+		}
+		return jButton5;
+	}
+
+	private JButton getOkButton() {
+		if (okButton == null) {
+			okButton = new JButton();
+			okButton.addActionListener(new ActionListener() {
+
+				public void actionPerformed(ActionEvent event) {
+					okButtonActionActionPerformed(event);
+				}
+			});
+			okButton.setText("Gerai");
+		}
+		return okButton;
+	}
+
 	private JPanel getJPanel1() {
 		if (jPanel1 == null) {
 			jPanel1 = new JPanel();
@@ -88,87 +253,12 @@ public class GUI extends JFrame {
 		return jPanel1;
 	}
 
-	private JButton getJButton5() {
-		if (jButton5 == null) {
-			jButton5 = new JButton();
-			jButton5.addActionListener(new ActionListener() {
-	
-				public void actionPerformed(ActionEvent event) {
-					ok2ButtonActionActionPerformed(event);
-				}
-			});
-			jButton5.setText("Gerai");
-		}
-		return jButton5;
-	}
-
 	private JScrollPane getJScrollPane1() {
 		if (jScrollPane1 == null) {
 			jScrollPane1 = new JScrollPane();
 			jScrollPane1.setViewportView(getRegistersTable());
 		}
 		return jScrollPane1;
-	}
-
-	private JButton getOkButton() {
-		if (okButton == null) {
-			okButton = new JButton();
-			okButton.addActionListener(new ActionListener() {
-	
-				public void actionPerformed(ActionEvent event) {
-					okButtonActionActionPerformed(event);
-				}
-			});
-			okButton.setText("Gerai");
-		}
-		return okButton;
-	}
-
-	private JPanel getJPanel0() {
-		if (jPanel0 == null) {
-			jPanel0 = new JPanel();
-			jPanel0.setLayout(new GroupLayout());
-			jPanel0.add(getJLabel1(), new Constraints(new Leading(8, 10, 10), new Leading(3, 10, 10)));
-			jPanel0.add(getJTextField0(), new Constraints(new Leading(62, 34, 12, 12), new Leading(3, 12, 12)));
-			jPanel0.add(getJTextField1(), new Constraints(new Leading(63, 12, 12), new Leading(27, 10, 10)));
-			jPanel0.add(getJLabel2(), new Constraints(new Leading(7, 12, 12), new Leading(29, 12, 12)));
-			jPanel0.add(getOkButton(), new Constraints(new Leading(18, 10, 10), new Leading(53, 12, 12)));
-		}
-		return jPanel0;
-	}
-
-	private JTextField getJTextField1() {
-		if (jTextField1 == null) {
-			jTextField1 = new JTextField();
-			jTextField1.setDocument(new JTextFieldLimit(4));
-			jTextField1.setText("FFFF");
-		}
-		return jTextField1;
-	}
-
-	private JLabel getJLabel2() {
-		if (jLabel2 == null) {
-			jLabel2 = new JLabel();
-			jLabel2.setText("Reikðmë");
-		}
-		return jLabel2;
-	}
-	
-	private JLabel getJLabel1() {
-		if (jLabel1 == null) {
-			jLabel1 = new JLabel();
-			jLabel1.setText("Adresas");
-		}
-		return jLabel1;
-	}
-
-	private JTextField getJTextField0() {
-		if (jTextField0 == null) {
-			jTextField0 = new JTextField(4);
-			jTextField0.setDocument(new JTextFieldLimit(4));
-			jTextField0.setText("0000");
-		}
-		return jTextField0;
 	}
 
 	private JScrollPane getJScrollPane5() {
@@ -198,11 +288,11 @@ public class GUI extends JFrame {
 	private JTable getCommandsTable() {
 		if (commandsTable == null) {
 			commandsTable = new JTable();
-			commandsTable.setModel(new DefaultTableModel(new Object[][] { { "ADD", "0", "0", "0", "0", }, { "SUB", "0", "0", "0", "0", }, },
-					new String[] { "K", "1", "2", "3", "4", }) {
+			commandsTable.setModel(new DefaultTableModel(new Object[][] { { "ADD", "0000", }, { "SUB", "0001",} },
+					new String[] { "Komanda", "Adresas", }) {
 				private static final long serialVersionUID = 1L;
-				Class<?>[] types = new Class<?>[] { Object.class, Object.class, String.class, String.class, String.class, };
-	
+				Class<?>[] types = new Class<?>[] { String.class, String.class, };
+
 				public Class<?> getColumnClass(int columnIndex) {
 					return types[columnIndex];
 				}
@@ -216,7 +306,7 @@ public class GUI extends JFrame {
 			playButton = new JButton();
 			playButton.setText("Vykdyti");
 			playButton.addActionListener(new ActionListener() {
-	
+
 				public void actionPerformed(ActionEvent event) {
 					playButtonActionActionPerformed(event);
 				}
@@ -228,9 +318,9 @@ public class GUI extends JFrame {
 	private JButton getJButton0() {
 		if (loadVMButton == null) {
 			loadVMButton = new JButton();
-			loadVMButton.setText("Ákelti VM");
+			loadVMButton.setText("?kelti VM");
 			loadVMButton.addActionListener(new ActionListener() {
-	
+
 				public void actionPerformed(ActionEvent event) {
 					loadVMButtonActionActionPerformed(event);
 				}
@@ -250,15 +340,16 @@ public class GUI extends JFrame {
 	private JTable getFlagsTable() {
 		if (flagsTable == null) {
 			flagsTable = new JTable();
-			flagsTable.setModel(new DefaultTableModel(new Object[][] { { "X", "0", }, { "X", "0", }, { "BF", "0", }, { "CF", "0", }, { "ZF", "0", },
-					{ "SF", "0", }, { "JF", "0", }, { "MF", "0", }, }, new String[] { "", "", }) {
+			flagsTable.setModel(new DefaultTableModel(new String[][] { { "X", "0", }, { "X", "0", }, { "BF", "0", }, { "CF", "0", }, { "ZF", "0", },
+					{ "SF", "0", }, { "JF", "0", }, { "MF", "0", }, }, new String[] { "FL", "B", }) {
 				private static final long serialVersionUID = 1L;
-				Class<?>[] types = new Class<?>[] { Object.class, Object.class, };
-	
+				Class<?>[] types = new Class<?>[] { String.class, String.class, };
+
 				public Class<?> getColumnClass(int columnIndex) {
 					return types[columnIndex];
 				}
 			});
+			flagsTable.setEnabled(false);
 		}
 		return flagsTable;
 	}
@@ -266,9 +357,9 @@ public class GUI extends JFrame {
 	private JButton getJButton4() {
 		if (changeMemoryButton == null) {
 			changeMemoryButton = new JButton();
-			changeMemoryButton.setText("Keisti atmintá");
+			changeMemoryButton.setText("Keisti atmint?");
 			changeMemoryButton.addActionListener(new ActionListener() {
-	
+
 				public void actionPerformed(ActionEvent event) {
 					changeMemoryButtonActionActionPerformed(event);
 				}
@@ -282,7 +373,7 @@ public class GUI extends JFrame {
 			changeRegistersButton = new JButton();
 			changeRegistersButton.setText("Keisti registrus");
 			changeRegistersButton.addActionListener(new ActionListener() {
-	
+
 				public void actionPerformed(ActionEvent event) {
 					changeRegistersButtonActionActionPerformed(event);
 				}
@@ -311,11 +402,11 @@ public class GUI extends JFrame {
 		if (registersTable == null) {
 			registersTable = new JTable();
 			registersTable.setModel(new DefaultTableModel(new Object[][] { { "PTR", 0, 0, 0, 0, }, { "R", 0, 0, 0, 0, }, { "P", 0, 0, 0, 0, },
-					{ "IC", 0, 0, 0, 0, }, { "SF", 0, 0, 0, 0, }, { "SM", 0, 0, 0, 0, }, { "PI", 0, 0, 0, 0, }, { "SI", 0, 0, 0, 0, }, { "CHNL", 0, 0, 0, 0, }, },
+					{ "IC", 0, 0, }, { "SF", 0, 0, }, { "SM", 0, 0, }, { "PI", 0}, { "SI", 0 }, { "CHNL", 0}, },
 					new String[] { "Reg", "1", "2", "3", "4", }) {
 				private static final long serialVersionUID = 1L;
 				Class<?>[] types = new Class<?>[] { Object.class, Integer.class, Integer.class, Integer.class, Integer.class, };
-	
+
 				public Class<?> getColumnClass(int columnIndex) {
 					return types[columnIndex];
 				}
@@ -339,11 +430,12 @@ public class GUI extends JFrame {
 			realMemoryTable.setModel(new DefaultTableModel(values, new String[] { "Adr", "1", "2", "3", "4", }) {
 				private static final long serialVersionUID = 1L;
 				Class<?>[] types = new Class<?>[] { String.class, String.class, String.class, String.class, String.class, };
-	
+
 				public Class<?> getColumnClass(int columnIndex) {
 					return types[columnIndex];
 				}
 			});
+			realMemoryTable.setEnabled(false);
 		}
 		return realMemoryTable;
 	}
@@ -351,9 +443,9 @@ public class GUI extends JFrame {
 	private JButton getJButton2() {
 		if (stepButton == null) {
 			stepButton = new JButton();
-			stepButton.setText("Þingsnis");
+			stepButton.setText("?ingsnis");
 			stepButton.addActionListener(new ActionListener() {
-	
+
 				public void actionPerformed(ActionEvent event) {
 					stepButtonActionActionPerformed(event);
 				}
@@ -364,7 +456,7 @@ public class GUI extends JFrame {
 
 	private void changeRegistersButtonActionActionPerformed(ActionEvent event) {
 		if(framek == null){
-			framek = new JFrame("Registrø Keitimas");
+			framek = new JFrame("Registr¸ Keitimas");
 			framek.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			registersTable.setEnabled(true);;
 			framek.getContentPane().add(getJPanel1(), BorderLayout.CENTER);
@@ -376,7 +468,7 @@ public class GUI extends JFrame {
 			jScrollPane1.setViewportView(getRegistersTable());
 			framek.setVisible(true);
 		}
-		
+
 	}
 
 	private void changeMemoryButtonActionActionPerformed(ActionEvent event) {
@@ -384,7 +476,7 @@ public class GUI extends JFrame {
 			frameks = new JFrame("Atminties Keitimas");
 			frameks.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			realMemoryTable.setEnabled(true);
-			frameks.getContentPane().add(getJPanel0(), BorderLayout.CENTER);
+			frameks.getContentPane().add(getJPanel2(), BorderLayout.CENTER);
 			frameks.pack();
 			frameks.setLocation(getLocation());
 			frameks.setVisible(true);
@@ -392,7 +484,7 @@ public class GUI extends JFrame {
 			frameks.setVisible(true);
 		}
 	}
-	
+
 	private void ok2ButtonActionActionPerformed(ActionEvent event) {
 		framek.setVisible(false);
 		getRegistersTable().setEnabled(false);
@@ -401,8 +493,10 @@ public class GUI extends JFrame {
 
 	private void okButtonActionActionPerformed(ActionEvent event){
 		frameks.setVisible(false);
-		Integer value = Integer.parseInt(jTextField1.getText());
-		realMemoryTable.setValueAt(value, Integer.parseInt(jTextField0.getText()), 1);
+		realMemoryTable.setValueAt(jTextField5.getText(), Integer.parseInt(jTextField6.getText()), 1);
+		realMemoryTable.setValueAt(jTextField2.getText(), Integer.parseInt(jTextField6.getText()), 2);
+		realMemoryTable.setValueAt(jTextField3.getText(), Integer.parseInt(jTextField6.getText()), 3);
+		realMemoryTable.setValueAt(jTextField4.getText(), Integer.parseInt(jTextField6.getText()), 4);
 	}
 	private void stepButtonActionActionPerformed(ActionEvent event) {
 	}
